fc8467bb5ddb0e79af4e88ccdfe71e07febe3900
R3M1d4Cod3r <superchristian9@gmail.com>
2024-12-14 16:10:27 +0100

    fix: imports

--- eval/evalAnomaly.py
+++ eval/evalAnomaly.py
@@ -11,7 +11,7 @@ import os.path as osp
 from argparse import ArgumentParser
 from ood_metrics import fpr_at_95_tpr, calc_metrics, plot_roc, plot_pr,plot_barcode
 from sklearn.metrics import roc_auc_score, roc_curve, auc, precision_recall_curve, average_precision_score
-from ....temperature_scaling import ModelWithTemperature
+from AnomalySegmentation.temperature_scaling import ModelWithTemperature
 
 import torch.nn.functional as F # aggiunto io 
 from torchvision.transforms import Compose, ToTensor, Normalize, Resize #aggiunto io
