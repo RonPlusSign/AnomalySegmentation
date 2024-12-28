6a6a79d3865b68677a5e574e03dfcf6d62373907
R3M1d4Cod3r <superchristian9@gmail.com>
2024-12-28 17:06:54 +0100

    fix: loss possbile

--- train/main.py
+++ train/main.py
@@ -75,7 +75,8 @@ class CrossEntropyLoss2d(torch.nn.Module):
     def __init__(self, weight=None):
         super().__init__()
 
-        self.loss = torch.nn.NLLLoss2d(weight)
+        #self.loss = torch.nn.NLLLoss2d(weight)
+        self.loss = torch.nn.NLLLoss(weight)
 
     def forward(self, outputs, targets):
         return self.loss(torch.nn.functional.log_softmax(outputs, dim=1), targets)
