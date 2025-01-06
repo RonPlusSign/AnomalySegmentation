99d02a1ad82557e3b484f6fd1e75a0fd4580a58b
giorgiamodi <giorgia.modi@stud.unifi.it>
2025-01-06 17:34:41 +0100

    fix

--- train/augmentations.py
+++ train/augmentations.py
@@ -102,11 +102,10 @@ class RandomResizedCrop(object):
         self.scales = scales
         self.size = size
 
-    def __call__(self, im_lb):
+    def __call__(self, im, lb):
         if self.size is None:
-            return im_lb
+            return im, lb
 
-        im, lb = im_lb['im'], im_lb['lb']
         assert im.shape[:2] == lb.shape[:2]
 
         crop_h, crop_w = self.size
@@ -115,7 +114,8 @@ class RandomResizedCrop(object):
         im = cv2.resize(im, (im_w, im_h))
         lb = cv2.resize(lb, (im_w, im_h), interpolation=cv2.INTER_NEAREST)
 
-        if (im_h, im_w) == (crop_h, crop_w): return dict(im=im, lb=lb)
+        if (im_h, im_w) == (crop_h, crop_w):
+            return im, lb
         pad_h, pad_w = 0, 0
         if im_h < crop_h:
             pad_h = (crop_h - im_h) // 2 + 1
@@ -128,11 +128,10 @@ class RandomResizedCrop(object):
         im_h, im_w, _ = im.shape
         sh, sw = np.random.random(2)
         sh, sw = int(sh * (im_h - crop_h)), int(sw * (im_w - crop_w))
-        return dict(
-            im=im[sh:sh+crop_h, sw:sw+crop_w, :].copy(),
-            lb=lb[sh:sh+crop_h, sw:sw+crop_w].copy()
+        return (
+            im[sh:sh+crop_h, sw:sw+crop_w, :].copy(),
+            lb[sh:sh+crop_h, sw:sw+crop_w].copy()
         )
-
 
 
 class RandomHorizontalFlip(object):
@@ -140,30 +139,24 @@ class RandomHorizontalFlip(object):
     def __init__(self, p=0.5):
         self.p = p
 
-    def __call__(self, im_lb):
+    def __call__(self, im, lb):
         if np.random.random() < self.p:
-            return im_lb
-        im, lb = im_lb['im'], im_lb['lb']
+            return im, lb
         assert im.shape[:2] == lb.shape[:2]
-        return dict(
-            im=im[:, ::-1, :],
-            lb=lb[:, ::-1],
-        )
-
+        return im[:, ::-1, :], lb[:, ::-1]
 
 
 class ColorJitter(object):
 
     def __init__(self, brightness=None, contrast=None, saturation=None):
         if not brightness is None and brightness >= 0:
-            self.brightness = [max(1-brightness, 0), 1+brightness]
+            self.brightness = [max(1 - brightness, 0), 1 + brightness]
         if not contrast is None and contrast >= 0:
-            self.contrast = [max(1-contrast, 0), 1+contrast]
+            self.contrast = [max(1 - contrast, 0), 1 + contrast]
         if not saturation is None and saturation >= 0:
-            self.saturation = [max(1-saturation, 0), 1+saturation]
+            self.saturation = [max(1 - saturation, 0), 1 + saturation]
 
-    def __call__(self, im_lb):
-        im, lb = im_lb['im'], im_lb['lb']
+    def __call__(self, im, lb):
         assert im.shape[:2] == lb.shape[:2]
         if not self.brightness is None:
             rate = np.random.uniform(*self.brightness)
@@ -174,16 +167,16 @@ class ColorJitter(object):
         if not self.saturation is None:
             rate = np.random.uniform(*self.saturation)
             im = self.adj_saturation(im, rate)
-        return dict(im=im, lb=lb,)
+        return im, lb
 
     def adj_saturation(self, im, rate):
         M = np.float32([
-            [1+2*rate, 1-rate, 1-rate],
-            [1-rate, 1+2*rate, 1-rate],
-            [1-rate, 1-rate, 1+2*rate]
+            [1 + 2 * rate, 1 - rate, 1 - rate],
+            [1 - rate, 1 + 2 * rate, 1 - rate],
+            [1 - rate, 1 - rate, 1 + 2 * rate]
         ])
         shape = im.shape
-        im = np.matmul(im.reshape(-1, 3), M).reshape(shape)/3
+        im = np.matmul(im.reshape(-1, 3), M).reshape(shape) / 3
         im = np.clip(im, 0, 255).astype(np.uint8)
         return im
 
@@ -200,8 +193,6 @@ class ColorJitter(object):
         return table[im]
 
 
-
-
 class ToTensor(object):
     '''
     mean and std should be of the channel order 'bgr'
@@ -210,17 +201,16 @@ class ToTensor(object):
         self.mean = mean
         self.std = std
 
-    def __call__(self, im_lb):
-        im, lb = im_lb['im'], im_lb['lb']
+    def __call__(self, im, lb):
         im = im.transpose(2, 0, 1).astype(np.float32)
         im = torch.from_numpy(im).div_(255)
         dtype, device = im.dtype, im.device
         mean = torch.as_tensor(self.mean, dtype=dtype, device=device)[:, None, None]
         std = torch.as_tensor(self.std, dtype=dtype, device=device)[:, None, None]
         im = im.sub_(mean).div_(std).clone()
-        if not lb is None:
+        if lb is not None:
             lb = torch.from_numpy(lb.astype(np.int64).copy()).clone()
-        return dict(im=im, lb=lb)
+        return im, lb
 
 
 class Compose(object):
@@ -228,7 +218,7 @@ class Compose(object):
     def __init__(self, do_list):
         self.do_list = do_list
 
-    def __call__(self, im_lb):
+    def __call__(self, im, lb):
         for comp in self.do_list:
-            im_lb = comp(im_lb)
-        return im_lb
+            im, lb = comp(im, lb)
+        return im, lb
