b102a1e61c355d6dfff2cacc2fb48d9095278cfe
giorgiamodi <giorgia.modi@stud.unifi.it>
2025-01-06 13:04:01 +0100

    add file augmentations.py

--- train/main.py
+++ train/main.py
@@ -4,7 +4,6 @@
 #######################
 
 import os
-import random
 import time
 import numpy as np
 import torch
@@ -36,6 +35,7 @@ from train.losses.cross_entropy_loss import CrossEntropyLoss2d
 from train.losses.logit_norm_loss import LogitNormLoss
 from train.losses.focal_loss import FocalLoss
 from train.losses.ohem_ce_loss import OhemCELoss
+from train.augmentations import MyCoTransform
 
 NUM_CHANNELS = 3
 NUM_CLASSES = 20  # 19 classes + void
@@ -43,41 +43,6 @@ NUM_CLASSES = 20  # 19 classes + void
 color_transform = Colorize(NUM_CLASSES)
 image_transform = ToPILImage()
 
-#Augmentations - different function implemented to perform random augments on both image and target
-class MyCoTransform(object):
-    def __init__(self, enc, augment=True, height=512):
-        self.enc=enc
-        self.augment = augment
-        self.height = height
-        pass
-    def __call__(self, input, target):
-        # do something to both images
-        input =  Resize(self.height, Image.BILINEAR)(input)
-        target = Resize(self.height, Image.NEAREST)(target)
-
-        if(self.augment):
-            # Random hflip
-            hflip = random.random()
-            if (hflip < 0.5):
-                input = input.transpose(Image.FLIP_LEFT_RIGHT)
-                target = target.transpose(Image.FLIP_LEFT_RIGHT)
-            
-            #Random translation 0-2 pixels (fill rest with padding
-            transX = random.randint(-2, 2) 
-            transY = random.randint(-2, 2)
-
-            input = ImageOps.expand(input, border=(transX,transY,0,0), fill=0)
-            target = ImageOps.expand(target, border=(transX,transY,0,0), fill=255) #pad label filling with 255
-            input = input.crop((0, 0, input.size[0]-transX, input.size[1]-transY))
-            target = target.crop((0, 0, target.size[0]-transX, target.size[1]-transY))   
-
-        input = ToTensor()(input)
-        if (self.enc):
-            target = Resize(int(self.height/8), Image.NEAREST)(target)
-        target = ToLabel()(target)
-        target = Relabel(255, 19)(target)
-
-        return input, target
     
 def calculate_class_weights(dataloader, num_classes, c=1.02):
     """Computes class weights as described in the ENet paper:
